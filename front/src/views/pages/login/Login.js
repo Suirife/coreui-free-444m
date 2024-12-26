7b9471a128d81bb4dc30c74da2c6cc799e940c07
Suirife <99396087+Suirife@users.noreply.github.com>
2024-12-26 15:38:17 +0700

    Update Login.js

--- front/src/views/pages/login/Login.js
+++ front/src/views/pages/login/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('${BASE_DOMAIN}/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
